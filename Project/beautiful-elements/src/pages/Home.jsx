6c164a05d51f8bcfdca138646499774055c7a419
SirawithPra <sirawith.pra@gmail.com>
2024-06-21 02:55:48 +0700

    element analog clock

--- Project/beautiful-elements/src/pages/Home.jsx
+++ Project/beautiful-elements/src/pages/Home.jsx
@@ -2,20 +2,25 @@ import React from 'react'
 import Glowy_buttons from '../components/Glowy_buttons/Glowy_buttons.jsx'
 import Reflect_button from '../components/Reflect_button/Reflect_button.jsx'
 import Hue_background from '../components/Hue_background/Hue_background.jsx'
+import Analog_clock from '../components/Analog_clock/Analog_clock.jsx'
 const Home = () => {
     return (
         <div>
-            <div>
+            <div className='main-container'>
                 <p>Glowy Button</p>
                 <Glowy_buttons />
             </div>
-            <div>
+            <div className='main-container'>
                 <p>Reflect_button</p>
                 <Reflect_button />
             </div>
-            <div>
+            <div className='main-container'>
                 <p>Hue_background</p>
                 <Hue_background />
+            </div>
+            <div className='main-container'>
+                <p>Analog_clock</p>
+                <Analog_clock/>
             </div>
         </div>
     )
