04cc65337202f73f3542e9730cc646b79779ead1
SirawithPra <sirawith.pra@gmail.com>
2024-08-11 17:03:43 +0700

    update hw

--- Project/Food-Deli/backend/config/db.js
+++ Project/Food-Deli/backend/config/db.js
@@ -1,19 +1,15 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://sirawith:Aai08m121518AA@cluster0.dfmwyb2.mongodb.net/food-deli').then(() => console.log('DB connected'));
-}
-// import mongoose from 'mongoose';
+export const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://sirawith:Aai08m121518AA@cluster0.dfmwyb2.mongodb.net/food-deli', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('DB connected');
+    } catch (error) {
+        console.error('DB connection failed:', error.message);
+        process.exit(1); // ปิดการทำงานของแอปพลิเคชันหากการเชื่อมต่อล้มเหลว
+    }
+};
 
-// export const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URI, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//         });
-//         console.log('DB connected');
-//     } catch (error) {
-//         console.error('DB connection error:', error);
-//         process.exit(1);
-//     }
-// };
